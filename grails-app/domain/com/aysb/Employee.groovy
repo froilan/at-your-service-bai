e46c8b7a918d5d84b10aa516094a526cee85f46f
Froilan <froilan.co@orangeandbronze.com>
2013-06-08 12:41:59 +0800

    Added photo in employee.
    Signed-off-by: Froilan <froilan.co@orangeandbronze.com>

--- grails-app/domain/com/aysb/Employee.groovy
+++ grails-app/domain/com/aysb/Employee.groovy
@@ -12,14 +12,16 @@ class Employee {
 	Boolean negotiableRate
 	BigDecimal highRate
 	BigDecimal lowRate
+	byte[] photo
+	CompanyProfile companyProfile
 	
 	static hasMany = [clients : Client]
-	
     static constraints = {
 		name nullable: false, blank: false
 		contactNumber nullable: false, blank:false
 		location nullable: false, blank:false 
 		highRate nullable: true, blank:true
 		lowRate nullable: true, blank:true
+		companyProfile nullable: true, blank:true
     }
 }
